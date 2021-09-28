5d1261d6416c92998a2a58e5619c4250f5e34417
Kalkidan Alemu <kali.taye@gmail.com>
2021-09-27 22:12:03 -0400

    initial files and created component

---
+++ src/components/CounterButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function CounterButton() {
+  return (
+    <button className="CounterButton">You clicked me X amount of times</button>
+  );
+}
+export default CounterButton;
